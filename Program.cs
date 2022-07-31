f23f9a26d4b4f1f0eee51780b0733706e2480e40
Mikhail <misha-z@yandex.ru>
2022-07-31 18:24:07 +0300

    вторая задача

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// программа выводит двумерный массив
+void Printarray(double[,]matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+
+void Fillarray(double[,]matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(-10,10);
+        }
+    }
+
+}
+
+double[,] matrix = new double[3,4];
+
+Fillarray(matrix);
+Printarray(matrix);
